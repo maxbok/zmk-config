517450ecc489dfb20b2f2665c389f1bd0c143484
Maxime Bokobza <max@codehawks.eu>
2024-11-03 11:51:03 +0100

    Fix unscii font variable

--- config/boards/shields/nice_view_custom/widgets/status.c
+++ config/boards/shields/nice_view_custom/widgets/status.c
@@ -138,7 +138,7 @@ static void draw_bottom(lv_obj_t *widget, lv_color_t cbuf[], const struct status
     lv_draw_rect_dsc_t rect_black_dsc;
     init_rect_dsc(&rect_black_dsc, LVGL_BACKGROUND);
     lv_draw_label_dsc_t label_dsc;
-    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &LV_FONT_UNSCII_16, LV_TEXT_ALIGN_CENTER);
+    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &lv_font_unscii_16, LV_TEXT_ALIGN_CENTER);
 
     // Fill background
     lv_canvas_draw_rect(canvas, 0, 0, CANVAS_SIZE, CANVAS_SIZE, &rect_black_dsc);
